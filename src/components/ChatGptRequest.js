a51219dfccbccfed7d8a212ef8287e70d22ad64c
TaherCCG <taher.ccg@gmail.com>
2024-02-17 23:45:10 +0000

    refactor:Form

--- src/components/ChatGptRequest.js
+++ src/components/ChatGptRequest.js
@@ -1,6 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import appStyles from "../styles/ChatGptForm.module.css";
+import logo from "../assets/logo.png";
+import { Row, Container, Form, Button, Col, Alert } from "react-bootstrap";
 
-const ChatGptRequest= () => {
+const ChatGptRequest = () => {
 
   const [question, setQuestion] = useState("");
   const [response, setResponse] = useState("");
@@ -26,77 +29,108 @@ const ChatGptRequest= () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <br />
-        <label>
-          Relationship:
-          <select
-            value={relationship}
-            onChange={(e) => setRelationship(e.target.value)}
-          >
-            <option value="Wife">Wife</option>
-            <option value="Husband">Husband</option>
-            <option value="Friend">Friend</option>
-            <option value="Sibling">Sibling</option>
-            <option value="Parent">Parent</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Type of Person:
-          <select
-            value={personType}
-            onChange={(e) => setPersonType(e.target.value)}
-          >
-            <option value="Sporty">Sporty</option>
-            <option value="Intellectual">Intellectual</option>
-            <option value="Creative">Creative</option>
-            <option value="Adventurous">Adventurous</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Age:
-          <input
-            type="number"
-            value={age}
-            min="1"
-            max="100"
-            onChange={(e) => setAge(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Loves:
-          <select value={loves} onChange={(e) => setLoves(e.target.value)}>
-            <option value="Animals">Animals</option>
-            <option value="Books">Books</option>
-            <option value="Travel">Travel</option>
-            <option value="Music">Music</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Ask a Question:
-          <input
-            type="text"
-            value={question}
-            onChange={(e) => setQuestion(e.target.value)}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+
+    <Row className={appStyles.Row}>
+      <Col className="my-auto py-2 p-md-2" md={4}>
+        <Container className={`${appStyles.Content} p-4`}>
+          <h1 className={appStyles.Header}>Ask a Question</h1>
+
+          <Form onSubmit={handleSubmit}>
+
+            <br />
+            <Form.Group className={appStyles.FormGrp} controlId="relationship">
+              <Form.Label className={appStyles.FormLabel}>Relationship:  </Form.Label>
+              <select
+                className={appStyles.Input}
+                placeholder="Select Relationship"
+                name="relationship"
+                as={"select"}
+                value={relationship}
+                onChange={(e) => setRelationship(e.target.value)}
+              >
+                <option value="Wife" >Wife</option>
+                <option value="Husband">Husband</option>
+                <option value="Friend">Friend</option>
+                <option value="Sibling">Sibling</option>
+                <option value="Parent">Parent</option>
+              </select>
+            </Form.Group>
+
+            <br />
+
+            <Form.Group className={appStyles.FormGrp} controlId="personType">
+              <Form.Label className={appStyles.FormLabel}>Type of Person:</Form.Label>
+              <select
+                className={appStyles.Input}
+                placeholder="Select Person Type"
+                name="personType"
+                as={"select"}
+                value={personType}
+                onChange={(e) => setPersonType(e.target.value)}
+              >
+                <option value="Sporty">Sporty</option>
+                <option value="Intellectual">Intellectual</option>
+                <option value="Creative">Creative</option>
+                <option value="Adventurous">Adventurous</option>
+              </select>
+            </Form.Group>
+            <br />
+
+            <Form.Group className={appStyles.FormGrp} controlId="age">
+              <Form.Label className={appStyles.FormLabel}>Age:  </Form.Label>
+              <input
+                className={appStyles.Input}
+                type="number"
+                value={age}
+                min="1"
+                max="100"
+                onChange={(e) => setAge(e.target.value)}
+              />
+            </Form.Group>
+            <br />
+
+            <Form.Group className={appStyles.FormGrp} controlId="loves">
+              <Form.Label className={appStyles.FormLabel}>Loves:  </Form.Label>
+              <select
+                className={appStyles.Input}
+                placeholder="Select Loves"
+                name="loves"
+                as={"select"}
+                value={loves}
+                onChange={(e) => setLoves(e.target.value)}
+              >
+                <option value="Animals">Animals</option>
+                <option value="Books">Books</option>
+                <option value="Travel">Travel</option>
+                <option value="Music">Music</option>
+              </select>
+            </Form.Group>
+            <br />
+
+            <Form.Group className={appStyles.FormGrp} controlId="question">
+              <Form.Label className={appStyles.FormLabel}>Ask a Question:  </Form.Label>
+              <input
+                className={appStyles.Input}
+                type="text"
+                value={question}
+                onChange={(e) => setQuestion(e.target.value)}
+              />
+            </Form.Group>
+            
+            <Button type="submit" className={appStyles.Button}><img src={logo}/> Ask Cupid</Button>
+
+          </Form>
+        </Container>
+      </Col>
       <div>{response}</div>
-    </div>
+    </Row>
   );
 }
 export default ChatGptRequest;
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
