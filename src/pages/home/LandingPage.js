be5fe32266399dc9132b2a456bbeaaadfe5aaf83
HillaM <hilla.muraja@hotmail.com>
2024-02-17 12:42:06 +0000

    Delete landing page form

--- src/pages/home/LandingPage.js
+++ src/pages/home/LandingPage.js
@@ -1,33 +1,10 @@
-import React, {useState} from "react";
-import appStyles from "../../styles/SignInForm.module.css";
-import { Form, Container, Button } from "react-bootstrap";
+import React from "react";
+import { Container } from "react-bootstrap";
 import logo from "../../assets/logo.png";
 import styles from "../../styles/LandingPage.module.css";
 import { Link } from "react-router-dom";
 
 const LandingPage = () => {
-
-  const [questionData, setQuestionData] = useState({
-    question: ""});
-
-  const { question} = questionData;
-  
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   try {
-  //     await axios.post("/dj-rest-auth/login/", signInData);
-  //     history.push("/");
-  //   } catch (err) {
-  //     setErrors(err.response?.data);
-  //   }
-  // };
-
-  const handleChange = (event) => {
-    setQuestionData({
-      ...questionData,
-      [event.target.name]: event.target.value,
-    });
-  };
 
 
   return (
@@ -42,25 +19,6 @@ const LandingPage = () => {
           <Link className={styles.Link} to="/signin">
             Sign In
           </Link>
-
-          <Form onSubmit={""}>
-
-            <Form.Group className="mb-3 mt-3" controlId="questions">
-              <Form.Label className="d-none">Questions</Form.Label>
-              <Form.Select
-                  className={appStyles.Input}
-                  type="text"
-                  placeholder="Select a Question"
-                  name="questions"
-                  onChange={handleChange}>
-                <option value="gift">Perfect Gift</option>
-                <option value="movie">Good Movie Suggestion</option>
-                <option value="travel">Travel Destination</option>
-                <option value="music">Music they might like</option>
-              </Form.Select>
-            </Form.Group>
-            <Button variant="primary" type="submit">Submit</Button>
-          </Form>
 
           <div>
             <p> Generate Answer Here </p>
