510dca8b19bcad9f7d7bd2ff62e75bf28251330b
Her Fri <herbert.frichert@outlook.de>
2024-02-19 13:42:02 +0100

    added WIP image to Profile page

--- src/pages/WorkInProgress.js
+++ src/pages/WorkInProgress.js
@@ -1,7 +1,14 @@
 import React from "react";
+import appStyles from "../styles/WorkInProgress.module.css";
+import wip from "../assets/wip.png";
 
 const WorkInProgress = () => {
-  return <div>WorkInProgress</div>;
+  return(
+    <div>
+      <h1>Work in Progress</h1>
+      <img className={appStyles.Test} src={wip} />
+    </div>
+  )
 };
 
 export default WorkInProgress;
